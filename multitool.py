6af4ab3ada98a28bd3488ae252c6a0c97f1fa3d7
Reaper <109579027+ReaperSkid@users.noreply.github.com>
2022-07-20 08:57:37 +1000

    Create multitool.py

---
+++ multitool.py
@@ -0,0 +1,17 @@
+import colorama as * #Imports colorama to make text colorful
+colorama.init()
+
+print(Fore.YOURCOLOR)
+print("YOUR ASCII")
+
+Choice = int(input("Enter your option [-->]: "))
+
+if Choice == 1:
+
+if Choice == 2:
+
+if Choice == 3:
+
+if Choice == 4:
+
+if Choice == 5:
